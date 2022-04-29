81824b0087298347741c67196f3d63102345004a
Hibiki Sakuraoka <wbydo@users.noreply.github.com>
2022-04-30 02:15:05 +0900

    WBYD-92 deployスクリプト作成

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,5 +1,3 @@
-import * as dotenv from 'dotenv';
-
 import { HardhatUserConfig } from 'hardhat/config';
 
 import '@nomiclabs/hardhat-waffle';
@@ -9,7 +7,25 @@ import '@typechain/hardhat';
 
 import 'hardhat-watcher';
 
-dotenv.config();
+const MUMBAI_URL = process.env.MUMBAI_URL;
+if (MUMBAI_URL == null || MUMBAI_URL === '') {
+  throw new Error('MUMBAI_URL is empty');
+}
+
+const POLYGON_URL = process.env.POLYGON_URL;
+if (POLYGON_URL == null || POLYGON_URL === '') {
+  throw new Error('POLYGON_URL is empty');
+}
+
+const PRIVATE_KEY_STG = process.env.PRIVATE_KEY_STG;
+if (PRIVATE_KEY_STG == null || PRIVATE_KEY_STG === '') {
+  throw new Error('PRIVATE_KEY_STG is empty');
+}
+
+const PRIVATE_KEY_PROD = process.env.PRIVATE_KEY_PROD;
+if (PRIVATE_KEY_PROD == null || PRIVATE_KEY_PROD === '') {
+  throw new Error('PRIVATE_KEY_PROD is empty');
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -31,12 +47,13 @@ const config: HardhatUserConfig = {
 
   networks: {
     hardhat: {},
-    sampleChain: {
-      url: '',
-      chainId: 999999999999,
-      accounts: [
-        'aaaabbbbccccddddaaaabbbbccccddddaaaabbbbccccddddaaaabbbbccccdddd',
-      ],
+    mumbai: {
+      url: MUMBAI_URL,
+      accounts: [PRIVATE_KEY_STG],
+    },
+    polygon: {
+      url: POLYGON_URL,
+      accounts: [PRIVATE_KEY_PROD],
     },
   },
 
